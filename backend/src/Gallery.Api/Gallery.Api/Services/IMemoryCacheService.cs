bfea52d6ac63cbf55d5fc434fa2cc2a8573697d3
yhaspel <yhaspel@aligntech.com>
2023-12-10 12:57:19 +0200

    remove backend

--- backend/src/Gallery.Api/Gallery.Api/Services/IMemoryCacheService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Gallery.Api.Services
-{
-    public interface IMemoryCacheService
-    {
-        bool GetFromCache<T>(string key, out T result);
-        void SetCacheEntry<T>(string key, T value, TimeSpan expiry);
-    }
-}
--- backend/src/Gallery.Api/Gallery.Api/Services/IPicsumService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Gallery.Api.Services
-{
-    public interface IPicsumService
-    {
-        Task<IList<GalleryDto>> GetPageAsync(int page, int limit, CancellationToken cancellationToken);
-        Task<IList<GalleryDto>> GetRandomListBasedOnPagingAsync(int limit, CancellationToken cancellationToken);
-    }
-}
